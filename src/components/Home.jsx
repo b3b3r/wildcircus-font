00dc4320f4f25ef0f3546c1d6048e238fd5998ab
b3b3r <b3b3rseg@gmail.com>
2019-07-16 13:59:18 +0200

    ajout image home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import './Home.scss';
 
 function Home() {
   return (
-    <div className="Home">
+    <div className="Home container">
+      <div className="row">
+        <img className="acrobatie" src="/images/home.jpg" alt="acrobatie" />
+      </div>
       <NavLink to="Nos-cirques"><Button color="primary">Les cirques</Button>{' '}</NavLink>
       <NavLink to="Votre-cirque"><Button color="primary">Votre cirque</Button>{' '}</NavLink>
     </div>
