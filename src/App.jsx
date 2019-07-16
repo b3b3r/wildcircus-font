6b5da682f453034477410aa024bcd36d6bae94ff
b3b3r <b3b3rseg@gmail.com>
2019-07-16 13:25:58 +0200

    creation composant circus

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import BarNav from './components/Navbar';
+<<<<<<< Updated upstream
+=======
+import Home from './components/Home';
+import Circus from './containers/Circus';
+import { Switch, Route } from 'react-router-dom';
+>>>>>>> Stashed changes
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BarNav />
+<<<<<<< Updated upstream
+=======
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="Nos cirques" component={Circus} />
+      </Switch>
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ src/containers/Circus.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Circus extends Component {
+  render() {
+    return (
+      <div className="Circus">
+        <p>coucou</p>
+      </div>
+    );
+  }
+}
+
+export default Circus;
