24aa75a90a76432831529cf45c01dc4b77a4ac78
b3b3r <b3b3rseg@gmail.com>
2019-07-16 14:19:07 +0200

    creation sous dossiers admin et deplacement user

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import BarNav from './components/Navbar';
 import Home from './components/Home';
-import Circus from './containers/Circus';
-import YourCircus from './containers/YourCircus';
+import Circus from './containers/User/Circus';
+import YourCircus from './containers/User/YourCircus';
 import Footer from './components/Footer';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
