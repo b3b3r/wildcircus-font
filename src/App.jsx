c0476bf909b02631c5c62d3aa8dbe9517b799692
b3b3r <b3b3rseg@gmail.com>
2019-07-16 13:16:28 +0200

    intégration du router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import BarNav from './components/Navbar';
+import Home from './components/Home';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BarNav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
